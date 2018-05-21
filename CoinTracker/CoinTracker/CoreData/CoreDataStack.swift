f0e7f90af2f584f6ee95c9a071de1473a4a65e09
Oleg Ryasnoy <ryasnoy.oleg@gmail.com>
2018-05-21 17:03:02 +0300

    CoreDataStack

---
+++ CoinTracker/CoinTracker/CoreData/CoreDataStack.swift
@@ -0,0 +1,40 @@
+//
+//  CoreDataStack.swift
+//  CoinTracker
+//
+//  Created by Oleg Ryasnoy on 21.05.2018.
+//  Copyright © 2018 Oleg Ryasnoy. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+final class CoreDataStack {
+  
+  static let shared = CoreDataStack()
+  
+  lazy var persistentContainer: NSPersistentContainer = {
+    let container = NSPersistentContainer(name: "CoinTracker")
+    container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+      if let error = error as NSError? {
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+    })
+    return container
+  }()
+  
+  // MARK: - Core Data Saving support
+  
+  func saveContext () {
+    let context = persistentContainer.viewContext
+    if context.hasChanges {
+      do {
+        try context.save()
+      } catch {
+        let nserror = error as NSError
+        fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+      }
+    }
+  }
+  
+}
