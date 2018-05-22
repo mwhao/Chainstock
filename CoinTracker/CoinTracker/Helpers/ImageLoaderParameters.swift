2a7d1c74274cce21ca4bd2682b7f27c30cbcfc6a
Oleg Ryasnoy <ryasnoy.oleg@gmail.com>
2018-05-22 12:58:09 +0300

    .gitignore update

---
+++ CoinTracker/CoinTracker/Helpers/ImageLoaderParameters.swift
@@ -0,0 +1,9 @@
+//
+//  ImageLoaderParameters.swift
+//  CoinTracker
+//
+//  Created by Oleg Ryasnoy on 22.05.2018.
+//  Copyright © 2018 Oleg Ryasnoy. All rights reserved.
+//
+
+import Foundation
