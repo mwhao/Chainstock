5950a98bab8b436865cbcc8fd18eaac3896a20d2
Oleg Ryasnoy <ryasnoy.oleg@gmail.com>
2018-05-23 09:59:14 +0300

    Serialization Test Case

--- CoinTracker/CoinTrackerTests/CoinTrackerTests.swift
+++ CoinTracker/CoinTrackerTests/CoinTrackerTests.swift
@@ -10,27 +10,30 @@ import XCTest
 @testable import CoinTracker
 
 class CoinTrackerTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  func testDecode() {
+
+    do {
+      let result = try JSONDecoder().decode(RawServerResponse.self, from: mockData.data(using: .utf8)!)
+      print(result)
+    } catch {
+      fatalError(error.localizedDescription)
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  }
+  
+  
 }
+
+
---
+++ CoinTracker/CoinTrackerTests/Mock.swift
@@ -0,0 +1,43 @@
+//
+//  Mock.swift
+//  CoinTracker
+//
+//  Created by Oleg Ryasnoy on 23.05.2018.
+//  Copyright © 2018 Oleg Ryasnoy. All rights reserved.
+//
+
+import Foundation
+
+let mockData = """
+{
+"data": {
+"2137": {
+"id": 2137,
+"name": "Electroneum",
+"symbol": "ETN",
+"website_slug": "electroneum",
+"rank": 100,
+"circulating_supply": 7053000443.0,
+"total_supply": 7053000443.0,
+"max_supply": 21000000000.0,
+"quotes": {
+"USD": {
+"price": 0.0208645,
+"volume_24h": 649560.0,
+"market_cap": 147157328.0,
+"percent_change_1h": 0.37,
+"percent_change_24h": -6.47,
+"percent_change_7d": -8.22
+}
+},
+"last_updated": 1527057258
+}
+},
+"metadata": {
+"timestamp": 1527057284,
+"num_cryptocurrencies": 1615,
+"error": null
+}
+}
+"""
+
